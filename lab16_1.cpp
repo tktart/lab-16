fc43d09a886844184a3d30e4f08be0c9689b87bd
tktart <tonkow235@gmail.com>
2025-02-04 02:32:23 +0700

    o

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double *data, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(data + i) = rand() / (RAND_MAX + 1.0);
+    }
+}
+
+void showData(double *data,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << data[i];
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void findRowSum(const double *data,double *sum1,int N,int M){
+	for(int i = 0 ; i < N; i++){
+		sum1[i] = 0 ;
+		for(int j= 0 ; j < M; j++){
+			sum1[i] += data[i*M + j];
+		}
+	}
+}
+
+void findColSum(const double *data,double *sum2,int N,int M){
+	for(int j = 0; j < M; j++){
+		sum2[j] = 0 ;
+		for(int i = 0; i < N; i++){
+			sum2[j] += data[i*M + j] ;
+		}
+	}
+}
